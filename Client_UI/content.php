a4cca83a2fa6499755386c73d498781e37f413a6
Mr-Rong <42691849+Mr-Rong@users.noreply.github.com>
2022-07-20 12:33:49 +0800

    Delete Client_UI directory

--- Client_UI/content.php
+++
@@ -1,4 +0,0 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Latest compiled JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
